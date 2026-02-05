19f07d87774c42d1a81b5f588bf4df82977ce1c3
sk-zk <sk-zk@users.noreply.github.com>
2026-02-05 18:44:40 +0100

    Add function for fetching `GEOResources`

--- lookaround/proto/generate.bat
+++ lookaround/proto/generate.bat
@@ -1,2 +1,2 @@
-protoc --python_out=. Shared.proto PlaceRequest.proto PlaceResponse.proto GroundMetadataTile.proto
+protoc --python_out=. Shared.proto PlaceRequest.proto PlaceResponse.proto GroundMetadataTile.proto Resources.proto
 fix-protobuf-imports .
